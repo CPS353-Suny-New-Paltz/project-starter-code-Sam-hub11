bc712ad0a5495b1a0bb967786e655d31f5b3684d
Sam-hub11 <carranzs1@newpaltz.edu>
2025-09-07 22:35:50 -0400

    Fix syntax and style issues

--- src/project/annotations/CS353.java
+++ src/project/annotations/CS353.java
@@ -1,6 +1,7 @@
 package project.annotations;
 
 public class CS353 {
-	System.out.print("Hello World");
-
-}
+	   public static void main(String[] args) {
+	        System.out.println("Hello world");
+	    }
+	}
